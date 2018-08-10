8e419c26fbf4fedc91126bce953191dd3109e8f1
NEORIS\e-hrchairez <chairezxd@gmail.com>
2018-08-10 15:01:34 -0500

    cambios extra

--- FinalComedor/src/app/components/navbar/navbar.component.ts
+++ FinalComedor/src/app/components/navbar/navbar.component.ts
@@ -11,29 +11,5 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit() {
   }
-  homeSelected() {
-    if (this.itemSelected === 'home') {
-      return true;
-    }
-    return false;
-  }
-  historySelected() {
-    if (this.itemSelected === 'history') {
-      return true;
-    }
-    return false;
-  }
-  userSelected() {
-    if (this.itemSelected === 'user') {
-      return true;
-    }
-    return false;
-  }
-
-  profileSelected() {
-    if (this.itemSelected === 'profile') {
-      return true;
-    }
-    return false;
-  }
+ 
 }
