1f54d84955a695a4d5a41cba015697566893db15
RicardoSTovar <ricardo.sauceda95@gmail.com>
2018-08-10 13:53:38 -0500

    Roles

--- FinalComedor/src/app/service/authentication/authentication.service.ts
+++ FinalComedor/src/app/service/authentication/authentication.service.ts
@@ -1,26 +1,62 @@
 import { Injectable } from '@angular/core';
 import { AngularFireDatabase } from 'angularfire2/database';
 import { AngularFireAuth } from 'angularfire2/auth';
-import * as firebase from 'firebase/app';
+import { Observable } from 'rxjs';
+import {map} from 'rxjs/operators';
 
+  import {User as firebaseUser} from 'firebase';
+import { FirebaseApp } from '../../../../node_modules/angularfire2';
+import * as firebase from 'firebase/app';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  public usuario: Observable<firebaseUser>;
+  public userID: string;
+  constructor(public angularFireDatabase: AngularFireDatabase, public angularFireAuth: AngularFireAuth) {
+    this.usuario = angularFireAuth.authState;
+  }
 
-  constructor(public angularFireDatabase: AngularFireDatabase, public angularFireAuth: AngularFireAuth) { }
-
+  
   emailRegistration(email, password) {
     return this.angularFireAuth.auth.createUserAndRetrieveDataWithEmailAndPassword(email, password);
   }
 
-  Login(email, password) {
-    return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
+  
+
+ dataReturn() {
+   return this.angularFireAuth.authState.pipe(
+     map(usuario => this.angularFireDatabase.object(`users/${usuario.uid}`).valueChanges())
+   );
+    //  const uid = await this.angularFireAuth.auth.onAuthStateChanged(function(user) {
+    //   if (user!=null)  { 
+    //   this.userID = user.uid;
+    //   }
+    // });
+
+    //  let userId: string = "gE6Kav5m6qgaRI7FoNqUVG9wIW12";
+    //  console.log("NUMERO BUENO"+ this.userID);
+    //  this.angularFireDatabase.database.ref(`users/${userId}`).once('value').then(userDetailsAsObject => {
+    //   let userDetails = userDetailsAsObject.val() ? userDetailsAsObject.val() : {};
+    //   console.log(userDetails);
+    // }).catch(err => {
+    //   console.log('Error pulling /profile table inside functionName() inside componentName.component.ts');
+    //   console.log(err);
+    // });
+ 
   }
 
-  LoginCellPhone(cellPhone,code) {
-    return this.angularFireAuth.auth.signInWithPhoneNumber(cellPhone,code);
+ 
+
+  
+  Login(email, password) {
+    return this.angularFireAuth.auth.signInWithEmailAndPassword(email, password);
+
+  }
+
+  LoginCellPhone(cellPhone, code) {
+    return this.angularFireAuth.auth.signInWithPhoneNumber(cellPhone, code);
   }
 
   loginFacebook(){
